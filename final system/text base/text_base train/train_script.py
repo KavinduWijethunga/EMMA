3e2764f8f92eed0475b4dc596aceb4888571971b
RuviniKarunarathne <ruvini.karunaratna@gmail.com>
2023-05-10 22:11:28 +0530

    adding out of vocabulary token

--- final system/text base/text_base train/train_script.py
+++ final system/text base/text_base train/train_script.py
@@ -48,7 +48,8 @@ embedding_dim = 16
 max_len = 20
 oov_token = "<OOV>"
 
-tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_token) # adding out of vocabulary token
+# adding out of vocabulary token
+tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_token)
 tokenizer.fit_on_texts(training_sentences)
 word_index = tokenizer.word_index
 sequences = tokenizer.texts_to_sequences(training_sentences)
